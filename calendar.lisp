0e2c65e045bc02706bf34bd85f3d7a586f248780
Kyle Brown <picokyle@gmail.com>
2015-11-21 20:50:00 -0700

    add julian stub

--- calendar.lisp
+++ calendar.lisp
@@ -3,35 +3,39 @@
   (:export :calendar))
 (in-package :calendar)
 
-;; Later this will use Julian if the year is <= 1750
-
 (defconstant +months+ '(january february march april may june july
                         august september october november december))
 
-(defun jan-1-day (year)
-  (mod (+ 1
-          (* 5 (mod (1- year) 4))
-          (* 4 (mod (1- year) 100))
-          (* 6 (mod (1- year) 400)))
-       7))
+(defun jan-1-day (year calendar)
+  (ecase calendar
+    (:gregorian (mod (+ 1
+                        (* 5 (mod (1- year) 4))
+                        (* 4 (mod (1- year) 100))
+                        (* 6 (mod (1- year) 400)))
+                     7))
+    (:julian 0)))
 
-(defun days-in-month (month year)
-  (let ((leap-year-p (or (and (zerop (mod year 4))
-                              (not (zerop (mod year 100))))
-                         (zerop (mod year 400)))))
-    (case month
-      (january   31)
-      (february  (if leap-year-p 29 28))
-      (march     31)
-      (april     30)
-      (may       31)
-      (june      30)
-      (july      31)
-      (august    31)
-      (september 30)
-      (october   31)
-      (november  30)
-      (december  31))))
+(defun leap-year-p (calendar year)
+  (ecase calendar
+    (:gregorian (or (and (zerop (mod year 4))
+                         (not (zerop (mod year 100))))
+                    (zerop (mod year 400))))
+    (:julian (zerop (mod year 4)))))
+
+(defun days-in-month (month year calendar)
+  (case month
+    (january   31)
+    (february  (if (leap-year-p calendar year) 29 28))
+    (march     31)
+    (april     30)
+    (may       31)
+    (june      30)
+    (july      31)
+    (august    31)
+    (september 30)
+    (october   31)
+    (november  30)
+    (december  31)))
 
 ;; this is ugly but it works and I'm tired
 (defun months-per-row (screen-width)
@@ -57,17 +61,20 @@
     result))
 
 (defun build-year (year)
-  (loop for month in +months+
-     for days-in-month = (days-in-month month year)
-     and start = (jan-1-day year) then (mod (+ start days-in-month) 7)
+  (loop with calendar = (if (<= year 1750) :julian :gregorian)
+     for month in +months+
+     for days-in-month = (days-in-month month year calendar)
+     and start = (jan-1-day year calendar) then (mod (+ start days-in-month) 7)
      collecting
        (sublists (append (loop repeat start collect "  ")
                          (loop for day from 1 to days-in-month
                             collecting (format nil "~2d" day)))
                  7 "  ")))
 
-(defun print-weekdays (month-names)
-  (format t "~{~:[~;Su Mo Tu We Th Fr Sa~^  ~]~}~%" month-names))
+(defun print-weekdays (n-times)
+  (let ((weekdays "Su Mo Tu We Th Fr Sa"))
+    (loop repeat (1- n-times) do (format t "~a  " weekdays)
+       finally (write-line weekdays))))
 
 (defun print-month-names (month-names)
   ;; 20 = 7 days * 2 + 6 for 1 space between them
@@ -77,7 +84,7 @@
 
 (defun print-row-header (month-names)
   (print-month-names month-names)
-  (print-weekdays    month-names))
+  (print-weekdays    (length month-names)))
 
 (defun print-month-days (months)
   (loop while (some #'identity months) do
@@ -86,7 +93,10 @@
                   collect (or (pop (nth i months))
                               (loop repeat 7 collect "  "))))))
 
-(defun calendar (year &optional (screen-width 80))
+(defun get-year ()
+  (nth-value 5 (decode-universal-time (get-universal-time))))
+
+(defun calendar (&key (year (get-year)) (screen-width 80))
   (let* ((months-per-row (months-per-row screen-width))
          (calendar-width (+ (* months-per-row 20) (* (1- months-per-row) 2)))
          (month-names    (sublists +months+ months-per-row))
